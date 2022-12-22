3f17ff9b44f0fda13754918aad1f154bd77f15b1
Rogerio Tadim <rogeriotadim@gmail.com>
2022-12-22 19:23:16 +0000

    app: server: fix ctx db

---
+++ desafio01-client-server-api/server/test.go
@@ -0,0 +1,15 @@
+package server
+
+var CotacaoTest = NewCotacao(
+	"USD",
+	"BRL",
+	"Dólar Americano/Real Brasileiro",
+	"5.3471",
+	"5.2792",
+	"0.0446",
+	"0.84",
+	"5.322",
+	"5.3245",
+	"1671124497",
+	"2022-12-15 14:51:33",
+)
