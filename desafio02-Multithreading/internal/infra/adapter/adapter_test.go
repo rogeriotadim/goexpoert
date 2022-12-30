ef7026095fe28fb8d12f7542156e907174f72431
Rogerio Tadim <rogerio.tadim@dataprev.gov.br>
2022-12-30 16:00:07 -0300

    desafio02: test: include timeout tests

--- desafio02-Multithreading/internal/infra/adapter/adapter_test.go
+++ desafio02-Multithreading/internal/infra/adapter/adapter_test.go
@@ -4,40 +4,45 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/rogeriotadim/goexpoert/desafio02-Multithreading/internal/dto"
 	"github.com/rogeriotadim/goexpoert/desafio02-Multithreading/internal/infra"
 )
 
 const expected = "02206-000"
+const expectedTimeout = "context deadline exceeded"
 const urlApiCep = "https://cdn.apicep.com/file/apicep/" + expected + ".json"
 const urlViaCep = "http://viacep.com.br/ws/" + expected + "/json"
 
 func TestApiCep(t *testing.T) {
 	adapter := NewAdapter(infra.CreateContext(time.Second * 30))
 	retorno, err := adapter.GetCep(urlApiCep)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 	cep, err := dto.NewApiCep(retorno)
-	if err != nil {
-		t.Error(err)
-	}
-	if cep.Code != expected {
-		t.Errorf("cep não confere")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, expected, cep.Code)
+}
+
+func TestApiCepTimeout(t *testing.T) {
+	adapter := NewAdapter(infra.CreateContext(time.Nanosecond))
+	_, err := adapter.GetCep(urlApiCep)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, expectedTimeout)
 }
 
 func TestViaCep(t *testing.T) {
 	adapter := NewAdapter(infra.CreateContext(time.Second * 30))
 	retorno, err := adapter.GetCep(urlViaCep)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 	cep, err := dto.NewViaCEP(retorno)
-	if err != nil {
-		t.Error(err)
-	}
-	if cep.Cep != expected {
-		t.Errorf("cep não confere")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, expected, cep.Cep)
+}
+
+func TestViaCepTimeout(t *testing.T) {
+	adapter := NewAdapter(infra.CreateContext(time.Nanosecond))
+	_, err := adapter.GetCep(urlViaCep)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, expectedTimeout)
 }
